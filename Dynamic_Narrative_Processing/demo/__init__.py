61e40163346165afb1b72addae94ff79f7a8ea33
rose <rosasun689@gmail.com>
2025-05-20 21:25:05 +0800

    Delete folder Dynamic_Narrative_Processing

--- Dynamic_Narrative_Processing/demo/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Demo package for the Dynamic Narrative Processing system.
-Contains visualization and testing tools for the narrative processing pipeline.
-"""
-
-from .demo_processor import DemoProcessor
-
-__all__ = ['DemoProcessor'] 
